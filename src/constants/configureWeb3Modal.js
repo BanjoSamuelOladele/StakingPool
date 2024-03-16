30c6096c406a610b067f5cbaea3bd201a664415c
whitewizardd <o.samuel@native.semicolon.africa>
2024-03-16 01:03:32 +0100

    created the web3configurer

--- src/constants/configureWeb3Modal.js
+++ src/constants/configureWeb3Modal.js
@@ -3,15 +3,15 @@
 import { createWeb3Modal, defaultConfig } from '@web3modal/ethers/react'
 
 // 1. Get projectId
-const projectId = 'YOUR_PROJECT_ID'
+const projectId = '2de4980c05f2420388f22d3e9b7fc318'
 
 // 2. Set chains
-const mainnet = {
-  chainId: 1,
-  name: 'Ethereum',
+const sepolia = {
+  chainId: 11155111,
+  name: 'Sepolia',
   currency: 'ETH',
-  explorerUrl: 'https://etherscan.io',
-  rpcUrl: 'https://cloudflare-eth.com'
+  explorerUrl: 'https://sepolia.etherscan.io',
+  rpcUrl: 'https://sepolia.infura.io/v3/2de4980c05f2420388f22d3e9b7fc318'
 }
 
 // 3. Create a metadata object
@@ -22,23 +22,13 @@ const metadata = {
   icons: ['https://avatars.mywebsite.com/']
 }
 
-// 4. Create Ethers config
-const ethersConfig = defaultConfig({
-  /*Required*/
-  metadata,
-
-  /*Optional*/
-  enableEIP6963: true, // true by default
-  enableInjected: true, // true by default
-  enableCoinbase: true, // true by default
-  rpcUrl: '...' // used for the Coinbase SDK
-  defaultChainId: 1, // used for the Coinbase SDK
-}),
 
 // 5. Create a Web3Modal instance
-createWeb3Modal({
-  ethersConfig,
-  chains: [mainnet],
-  projectId,
-  enableAnalytics: true // Optional - defaults to your Cloud configuration
-})
+export const configureWeb3Modal = () =>{
+    createWeb3Modal({
+    ethersConfig: defaultConfig({metadata}),
+    chains: [sepolia],
+    projectId,
+    enableAnalytics: false // Optional - defaults to your Cloud configuration
+    })
+}
