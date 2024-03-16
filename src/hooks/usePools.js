f1e8bf867df99bfc34d3f5d763399231960f5272
whitewizardd <o.samuel@native.semicolon.africa>
2024-03-16 10:19:59 +0100

    updated

--- src/hooks/usePools.js
+++ src/hooks/usePools.js
@@ -20,19 +20,19 @@ const usePools = () => {
     useEffect(() => {
         let allPools = []
         try {
-            (async() => {
-            const res = await contract.id();
-            for (let index = 0; index < res; index++) {
-              const cc = await contract.getPoolByID(index);
-              const good = Object.create(poolers);
-              good.totalStakers=cc[0];
-              good.totalStaked=cc[1];
-              good.rewardReserve=cc[2];
-              good.rewardRate=cc[3];
-              allPools.push(good);
-            }
-            })()
-            setPools(allPools)
+                (async() => {
+                        const res = await contract.id();
+                        for (let index = 0; index < res; index++) {
+                            const cc = await contract.getPoolByID(index);
+                            const good = Object.create(poolers);
+                            good.totalStakers=cc[0];
+                            good.totalStaked=cc[1];
+                            good.rewardReserve=cc[2];
+                            good.rewardRate=cc[3];
+                            allPools.push(good);
+                        }
+                    })()
+                setPools(allPools)
         } catch (error) {
             console.error(error);
         }
